137469e456e3445f7816d11441519c0a09d75cac
Tiago Silva <tiagokost@gmail.com>
2026-06-26 23:52:52 -0300

    Initial commit

---
+++ code-common/src/main/java/br/com/codeteam/codecommon/CodeCommonApplication.java
@@ -0,0 +1,13 @@
+package br.com.codeteam.codecommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeCommonApplication.class, args);
+	}
+
+}
---
+++ code-common/src/test/java/br/com/codeteam/codecommon/CodeCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codeteam.codecommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
